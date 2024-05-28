747bf8bb480cf2c89103240676e64ff7f7730d1f
V-SpringT <vuxuanthinh2003@gmail.com>
2024-05-28 17:00:51 +0700

    test middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,17 +1,17 @@
 const User = require("../db/userModel");
 module.exports.hasSessionRecord = async (request, response, next)=>{
-    // if(request.cookies.user_id) {
-    //     const user = await User.findOne({_id: request.cookies.user_id})
-    //     if(user){
-    //         next(); 
-    //     }
-    //     else{
-    //         response.clearCookie('user_id');
-    //         response.status(400).send('Người dùng chưa đăng nhập');
-    //     }
-    // }
-    // else {
-    //     response.status(400).send('Người dùng chưa đăng nhập');
-    // }
-    next();
+    if(request.cookies.user_id) {
+        const user = await User.findOne({_id: request.cookies.user_id})
+        if(user){
+            next(); 
+        }
+        else{
+            console.log(request.cookies.user_id)
+            response.clearCookie('user_id');
+            response.status(400).send('Người dùng chưa đăng nhập');
+        }
+    }
+    else {
+        response.status(400).send('Người dùng chưa đăng nhập');
+    }
 }
